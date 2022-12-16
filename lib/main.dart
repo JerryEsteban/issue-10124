a2c9c1ca681b22e35d9bef1e90356266ec0d17be
Jerry Esteban <estebangs@mettamaker.com>
2022-12-16 09:51:41 -0800

    Code sample for issue #10124

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+import 'package:flutter/material.dart';
+import 'package:test_app/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return SignInScreen(
+              providers: [
+                GoogleProvider(
+                  clientId:
+                      '1032695919493-a88cou4167jkf1njrafk57vf79p09t35.apps.googleusercontent.com',
+                )
+              ],
+              actions: [
+                AuthStateChangeAction<UserCreated>((context, state) {
+                  // Not being triggered on first login with Google sign-in.
+                  print('User created!');
+                }),
+              ],
+            );
+          }
+
+          return const SignOutButton();
+        });
+  }
+}
